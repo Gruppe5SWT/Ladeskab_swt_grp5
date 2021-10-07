788762cec828d5b0e818138f71f60382c48f8bec
miemouridsen <miemouridsen@hotmail.com>
2021-10-07 10:39:28 +0200

    cs files moved

--- Ladeskab.Test.Unit/UnitTests.cs
+++ Ladeskab.Test.Unit/UnitTests.cs
@@ -1,9 +1,11 @@
 using NUnit.Framework;
+using UsbSimulator;
 
 namespace Ladeskab.Test.Unit
 {
     public class Tests
     {
+        private IUsbCharger charger;
         [SetUp]
         public void Setup()
         {

f24ba88ef5662a2cc6071a3a2366f7e307133c6a
Marc <marcbkonig@gmail.com>
2021-10-07 11:08:28 +0200

    add interfaces
    IChargeControl
    IDoor

---
+++ Ladeskab/Interfaces/IChargeControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Interfaces
+{
+    interface IChargeControl
+    {
+        bool Connected();
+        void StartCharge();
+        void StopCharge();
+    }
+}
---
+++ Ladeskab/Interfaces/IDoor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab.Interfaces
+{
+    interface IDoor
+    {
+        void LockDoor();
+        void UnlockDoor();
+    }
+
+}

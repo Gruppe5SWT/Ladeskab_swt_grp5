3c4f001887d499c7a9805c093d729deaf87c9c7a
technimeo <au591176@post.au.dk>
2021-10-07 11:29:30 +0200

    Created ChargeControl.cs, moved IUSBCharger to Interface folder, updated project dependencies

---
+++ Ladeskab/ChargeControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+using UsbSimulator;
+
+namespace Ladeskab
+{
+    class ChargeControl : IChargeControl
+    {
+        public bool Connected { get; set; }
+
+        public void StartCharge()
+        {
+            Console.WriteLine("Started charging");
+        }
+
+        public void StopCharge()
+        {
+            Console.WriteLine("Stopped charging");
+        }
+
+        private void HandleCurrentEvent(object sender, CurrentEventArgs e)
+        {
+
+        }
+
+        
+    }
+}

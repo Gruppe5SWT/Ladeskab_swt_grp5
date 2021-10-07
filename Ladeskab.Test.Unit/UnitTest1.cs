72c5db1591d583d9cccd0cc5d1f69ced02894d02
miemouridsen <miemouridsen@hotmail.com>
2021-10-07 10:58:29 +0200

    Projects changed to core 5.0

--- Ladeskab.Test.Unit/UnitTest1.cs
+++ Ladeskab.Test.Unit/UnitTest1.cs
@@ -1,11 +1,9 @@
 using NUnit.Framework;
-using UsbSimulator;
 
-namespace Ladeskab.Test.Unit
+namespace Ladeskab.Test.Unit_
 {
     public class Tests
     {
-        private IUsbCharger charger;
         [SetUp]
         public void Setup()
         {

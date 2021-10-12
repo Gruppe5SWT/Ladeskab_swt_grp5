7c22ef1ec02700c964dd9a4fd3e5456e8db316d6
miemouridsen <miemouridsen@hotmail.com>
2021-10-12 09:15:33 +0200

    Unittests for Door implemented

--- Ladeskab/Interfaces/IStationControl.cs
+++ Ladeskab/Interfaces/IStationControl.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ladeskab.Interfaces
 {
-    interface IStationControl
+    public interface IStationControl
     {
         void DoorOpened();
         void DoorClosed();

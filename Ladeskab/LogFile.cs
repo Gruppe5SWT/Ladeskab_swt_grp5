c8cd27fe897c1bdf4f33a725d4406a3da82d6c62
technimeo <au591176@post.au.dk>
2021-10-14 10:48:35 +0200

    Added LogFile class

---
+++ Ladeskab/LogFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+
+namespace Ladeskab
+{
+    class LogFile : ILogFile
+    {
+        public void LogDoorLocked()
+        {
+            FileStream fs = new FileStream("LogFile.txt", FileMode.OpenOrCreate);
+            StreamWriter s = new StreamWriter(fs);
+            s.WriteLine(DateTime.Now + ": Door Locked");
+            s.Close();
+            fs.Close();
+   
+        }
+
+        public void LogDoorUnlocked()
+        {
+            FileStream fs = new FileStream("LogFile.txt", FileMode.OpenOrCreate);
+            StreamWriter s = new StreamWriter(fs);
+            s.WriteLine(DateTime.Now + ": Door Unlocked");
+            s.Close();
+            fs.Close();
+
+        }
+    }
+}
